8041f3b853d39f89c59486ec7fced7b76c84a5c5
Calvin Chong <calvin@lenslockers.com>
2023-03-07 16:12:11 -0500

    Added serverside menu data request from Mongo

--- menumanagement/src/app/menu/page.js
+++ menumanagement/src/app/menu/page.js
@@ -7,11 +7,22 @@ import MenuLink from '../../components/MenuLink';
 import styles from './page.module.css';
 import Link from 'next/link';
 
+// fetch data from menu api here
+const getMenuDataFromMongo = async () => {
+  const res = await fetch('http://localhost:3000/api/menu', {
+    method: 'GET'
+  });
+
+  const data = await res.json();
+  return data;
+}
 
 const menuDivStyle = "border-solid border-indigo-500 border-2";
 
 const Menu = async () => {
   const items = await getMenuFixtures(100);
+  // const databaseItems = await getMenuDataFromMongo();
+  // console.log ('databaseItems', databaseItems);
 
   return (
     <div className={styles.menu}>
