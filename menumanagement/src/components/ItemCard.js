a2f46545b65539009e38980eca01c39265eea047
Calvin Chong <calvin@lenslockers.com>
2023-03-20 12:16:57 -0400

    Add AddtoCart functionality to Button

--- menumanagement/src/components/ItemCard.js
+++ menumanagement/src/components/ItemCard.js
@@ -4,8 +4,20 @@ import styles from '../styles/ItemCard.module.css';
 import { IoBagAdd } from 'react-icons/io5';
 import { IoAddCircleSharp } from 'react-icons/io5'
 
+
+
 const ItemCard = ({item}) => {
   const description = item.description.length === 0 ? null: item.description;
+
+  const addItemToLocalStorage = ( item ) => {
+    console.log('hello', item);
+    if (typeof window !== 'undefined') {
+      let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+      cart.push(item);
+      localStorage.setItem('cart', JSON.stringify(cart));
+      console.log(localStorage);
+    }
+  }
 
   return (
       <div className={styles.box}>
@@ -14,7 +26,10 @@ const ItemCard = ({item}) => {
           <p>{item.price}</p>
         </div>
         <p className={styles.description}>{description}</p>
-        <IoAddCircleSharp className={styles.cartButton} />
+        <IoAddCircleSharp
+          className={styles.cartButton}
+          onClick={addItemToLocalStorage(item)}
+        />
       </div>
   )
 }
