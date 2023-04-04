05e89bd42db225b11c3fccbc653adae261ea3af2
Calvin Chong <calvin@lenslockers.com>
2023-04-04 11:21:41 -0400

    Sticky Nav Bar and DB calls

--- menumanagement/src/lib/serversideDBCalls/getMenuDBSS.js
+++ menumanagement/src/lib/serversideDBCalls/getMenuDBSS.js
@@ -1,10 +1,11 @@
 import { db } from '../db/db.mjs';
 import { menuItems } from '../models/menuItems.mjs';
 
-// get menu serverside;
+// get menu serverside; database connection is made once when calling menu item
 // output-type: ARRAY:  returns a formatted array of menu objects || returns an empty array if connection to DB fails.
 export default async function getMenuDBSS() {
-  await db(); // connect to the database
+
+  await db(); // makes connection to the database. The only thing is I don't want to have it make multiple connections every time I make an API call/.
 
   try {
     const items = await menuItems.find({}); /* find all the data in our database */
