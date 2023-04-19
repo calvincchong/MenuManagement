1cdb9b3a51e8cf6ed942dc6b4e0d9e784ced04c2
Calvin Chong <calvin@lenslockers.com>
2023-04-19 13:36:38 -0400

    Test Accordion on Mobile Look

--- menumanagement/src/components/OrderingApp.js
+++ menumanagement/src/components/OrderingApp.js
@@ -12,23 +12,31 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectCategory } from '../store/reducers/menuCategorySlice';
 import { toggleCart } from '../store/reducers/cartSlice';
 import { selectItem } from '../store/reducers/selectItemSlice';
-import { useTransition, useSpring, animated, config, useSpringRef } from '@react-spring/web';
+import {
+  useTransition,
+  useSpring,
+  animated,
+  config,
+  useSpringRef,
+} from '@react-spring/web';
+import Accordion from './Accordion';
 // import Message from './AddToCartNotification';
 
-const headerTCSS = "flex relative justify-center place-content-center min-h-xxxvh bg-center bg-fixed bg-cover bg-[url(https://res.cloudinary.com/dq6rqplja/image/upload/v1678385134/Koo%20Koo%20Chicken/kkc-top-down-menu-item_seijj1.jpg)] min-h-fit min-h-20"
+const headerTCSS =
+  'flex relative justify-center place-content-center min-h-xxxvh bg-center bg-fixed bg-cover bg-[url(https://res.cloudinary.com/dq6rqplja/image/upload/v1678385134/Koo%20Koo%20Chicken/kkc-top-down-menu-item_seijj1.jpg)] min-h-fit min-h-20';
 
 // maps across the categories and returns a section for each set of items
-const OrderingApp = ({items, categories}) => {
-  const selectedCategory = useSelector((state) => state.menuCategory);
-  const isShowCart = useSelector((state) => state.cart);
-  const isShowItemDetails = useSelector((state) => state.item);
+const OrderingApp = ({ items, categories }) => {
+  const selectedCategory = useSelector(state => state.menuCategory);
+  const isShowCart = useSelector(state => state.cart);
+  const isShowItemDetails = useSelector(state => state.item);
   const dispatch = useDispatch();
   const transRef = useSpringRef();
   const trans = useTransition([isShowCart], {
     ref: transRef,
     keys: null,
     from: {
-      opacity: .5,
+      opacity: 0.5,
       transform: `scale(${0.9})`,
       // transformOrigin: 'top right'
     },
@@ -41,15 +49,14 @@ const OrderingApp = ({items, categories}) => {
     //   opacity: .5,
     //   transform: `scale(${0.9})`,
     // },
-    config: config.wobble
+    config: config.wobble,
   });
 
   useEffect(() => {
-    transRef.start()
+    transRef.start();
   }, [isShowCart]);
 
-
-  const filterCategories = (category) => {
+  const filterCategories = category => {
     if (selectedCategory.value === 'All Items') {
       return true;
     }
@@ -58,13 +65,12 @@ const OrderingApp = ({items, categories}) => {
 
   return (
     <>
-    {isShowItemDetails.value !== false ?
-      (<div>
-        hellooooo
-        <ItemDetails />
-      </div>)
-      : null}
-    {/* <div className={headerTCSS}>
+      {isShowItemDetails.value !== false ? (
+        <div>
+          <ItemDetails />
+        </div>
+      ) : null}
+      {/* <div className={headerTCSS}>
       <div className="absolute top-1/4 text-center">
         <div>
           <h1 className={tcss.h1white}>
@@ -79,48 +85,50 @@ const OrderingApp = ({items, categories}) => {
         </div>
       </div>
     </div> */}
-    {/* <div></div> */}
-    <div className={isShowCart ? styles.menuContainerWithCart : styles.menuContainerWithoutCart}>
-      <MenuFilter categories={categories} />
-      <div className='w-12/12 sm:w-9/12 md:w-9/12'>
-         {categories.filter(filterCategories).map((category, i) => {
+      {/* <div></div> */}
+      <div
+        className={
+          isShowCart
+            ? styles.menuContainerWithCart
+            : styles.menuContainerWithoutCart
+        }
+      >
+        <MenuFilter categories={categories} />
+        <div className="w-12/12 sm:w-9/12 md:w-9/12">
+          {categories.filter(filterCategories).map((category, i) => {
+            const itemsByCategory = items.filter(item => {
+              return item.category === category;
+            });
 
-          const itemsByCategory = items.filter(item => {
-            return item.category === category;
-          });
-
-          return (
-            <div id={category} className={styles.categorySections} key={`header-${i}`}>
-              <div className='py-2'>
-                <h2 className={tcss.h4}>{category}</h2>
+            return (
+              <div
+                id={category}
+                className={styles.categorySections}
+                key={`header-${i}`}
+              >
+                <div className="py-2">
+                  <h2 className={tcss.h4}>{category}</h2>
+                  <Accordion />
+                </div>
+                <CategoryItems items={itemsByCategory} />
               </div>
-              <CategoryItems items={itemsByCategory} />
-            </div>
-          )
-         })}
+            );
+          })}
+        </div>
+
+        {isShowCart.value ? (
+          <>
+            {trans((style, item) => (
+              <animated.div className={styles.slideout} style={{ ...style }}>
+                {<Slideout>{<CartDetails />}</Slideout>}
+              </animated.div>
+            ))}
+            <div className="w-[20vw]"></div>
+          </>
+        ) : null}
       </div>
-
-      {isShowCart.value ?
-        (<>
-          {trans((style, item) =>
-            <animated.div
-              className={styles.slideout}
-              style={{...style}}
-            >
-              {<Slideout>{<CartDetails />}</Slideout>}
-            </animated.div>
-          )}
-          <div className='w-[20vw]'></div>
-        </>)
-        :
-        null
-      }
-
-
-
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default OrderingApp;
