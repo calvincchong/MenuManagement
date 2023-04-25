c7527b7fb242cf9fbf90225c26104470ddbd51d5
Calvin Chong <calvin@lenslockers.com>
2023-04-25 16:44:46 -0400

    Beautified Edit Item Form

--- menumanagement/src/app/Providers.js
+++ menumanagement/src/app/Providers.js
@@ -2,14 +2,11 @@
 
 import { PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
-import store  from '../store/store';
+import store from '../store/store';
+// add any additional providers here
 
-const Providers = ({children}) => {
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  )
-}
+const Providers = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
 
 export default Providers;
