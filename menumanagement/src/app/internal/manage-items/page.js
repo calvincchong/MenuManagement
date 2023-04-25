ad4d4021ba9169499012bdbd52d784a387ae9412
Calvin Chong <calvin@lenslockers.com>
2023-04-25 15:43:46 -0400

    Update revalidation times on menu and item mgmt

--- menumanagement/src/app/internal/manage-items/page.js
+++ menumanagement/src/app/internal/manage-items/page.js
@@ -1,13 +1,16 @@
 import getMenuDBSS from '../../../lib/serversideDBCalls/getMenuDBSS';
 import ItemsTable from '../../../components/ItemsTable';
-
+import { headers } from 'next/headers';
 // Make call to retrieve menu data from database
 // Send all data down to the fields.
 
-const menuItems = await getMenuDBSS();
-const menuItems2 = JSON.parse(menuItems);
+export const revalidate = 0;
 
-const ManageItems = () => {
+const ManageItems = async () => {
+  const headersList = headers();
+  const menuItems = await getMenuDBSS();
+  const menuItems2 = JSON.parse(menuItems);
+
   return (
     <div>
       <ItemsTable items={menuItems2} />
