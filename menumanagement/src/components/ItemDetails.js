405754ebc08bddcde9c6dce72aedcfac2f1ce8ee
Calvin Chong <calvin@lenslockers.com>
2023-04-25 15:44:21 -0400

    Add  click + add to cart

--- menumanagement/src/components/ItemDetails.js
+++ menumanagement/src/components/ItemDetails.js
@@ -5,7 +5,7 @@ import styles from '../styles/ItemDetails.module.css';
 import Image from 'next/image';
 import { HiArrowsPointingIn } from 'react-icons/hi2';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
-
+import { addItemToCartAndSetStorage } from '../lib/controllers/calculateCart';
 // const myLoader = ({ src, width, quality }) => {
 //   return `https://example.com/${src}?w=${width}&q=${quality || 75}`
 // }
@@ -18,6 +18,19 @@ const ItemDetails = () => {
   const item = useSelector(state => state.item);
   const dispatch = useDispatch(); // need to set category items to state.
   // console.log('hello in item details', item.value.menuName);
+
+  // const addItemToLocalStorage = () => {
+  //   console.log('this is the item value', item.value);
+  //   if (typeof window !== 'undefined') {
+  //     let cart =
+  //       localStorage.getItem('cart') !== undefined
+  //         ? JSON.parse(localStorage.getItem('cart'))
+  //         : [];
+  //     cart = [...cart, item];
+  //     localStorage.setItem('cart', JSON.stringify(cart));
+  //     window.dispatchEvent(new Event('addItemToCart')); // Mechanism to update cart displaying number of items
+  //   }
+  // };
 
   return (
     <div className={styles.modalContainer}>
@@ -44,10 +57,17 @@ const ItemDetails = () => {
         <div className={styles.itemHeader}>
           {item !== null ? item.value.menuName : 'Salt & Pepper Squid 椒鹽鮮魷'}
         </div>
-        <div className="">{item !== null ? item.value.description : ''}</div>
+        <div className={styles['item-description']}>
+          {item !== null ? item.value.description : ''}
+        </div>
         {/* <div className=''>{item['options'] !== undefined ? item.options : '' }</div> */}
 
-        <button className={styles.addToCartButton}>
+        <button
+          onClick={() => {
+            addItemToCartAndSetStorage(item.value);
+          }}
+          className={styles.addToCartButton}
+        >
           {' '}
           Add {item.quantity} to Cart{' '}
         </button>
